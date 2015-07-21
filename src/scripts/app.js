a06a5a700e0c6e3269213ccdfe107c95bdce2160
Sonia Fabre <sonia.fabre@gmail.com>
2015-07-22 00:24:38 +0800

    Routing is no longer necessary

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -2,34 +2,15 @@
 /** @jsx React.DOM */
 var React = require('react');
 
-// routing
-var Router        = require('react-router');
-var RouteHandler  = Router.RouteHandler;
-var Route         = Router.Route;
-var DefaultRoute  = Router.DefaultRoute;
-
-// view components
 var ContactsList  = require('./components/contactsList');
-var AddContact    = require('./components/add');
-var EditContact   = require('./components/edit');
+var App = React.createClass({
 
-
-var routes = (
-  <Route handler={ ContactManager }>
-    <Route name="contact" path="/edit/:id" handler={ EditContact } />
-    <Route name="add" path="/new" handler={ AddContact } />
-    <DefaultRoute name="home" handler={ ContactsList } />
-  </Route>
-);
-
-var ContactManager = React.createClass({
   render: function() {
     return (
-      <RouteHandler/>
-    );
+          <ContactsList />
+      )
   }
 });
 
-Router.run(routes, function(Handler) {
-  React.render(<Handler/>, document.getElementById('app'));
-});
+React.render(<App />,
+  document.getElementById('app'));
